3ac2b66f086103e8d4103e9e64226165f3a6ac2f
jvo9 <vo-0008@jmss.vic.edu.au>
2015-09-19 20:14:30 +1000

    - 'Jeffrey_Open_File.py' has updated code, which is also commented. - For ease of access, I pulled the shortcut for the google doc containing the notes Dr McIver made during Ramesh's seconds visit into the project. - 'test_graph.csv' is the csv file that 'Jeffrey_Open_File.py' writes to, if you need to export the data into excel (running the program with the code uncommented and then externally saving the csv file as an excel file). - 'test_graph_excel.xlsx' is the excel version of 'test_graph.csv', containing some graphs and formulas (frequency) I played around with. I believe it contains the data for file 'f05', same as shown in the 'Jeffrey_Open_File.py' file.

--- Jeffrey_Open_File.py
+++ Jeffrey_Open_File.py
@@ -1,13 +1,58 @@
+# Scipy is necessry for opening the MatLab files
+# MatPlotLib is not used yet, but may be used to plot the data
+# CSV is so that the data could be written to excel
 import scipy.io as scio
 import matplotlib.pyplot as mpl
+import csv
 
+# Function task the name of the MatLab files (including the .mat at the end)
 def open_matlab_file(matlab_filename):
     # headers of relevant data = 'StimTrig', b
     mat = scio.loadmat(matlab_filename)
     stim_trig_raw = mat['StimTrig']
     for x in mat.keys():
-        if x != "__version__" and x != "__globals__" and x != "StimTrig" and x != "__header__":
+        # I assumed that Sch_wav and Schmitt would be used interchangebly thoughout the files
+        if x not in ("__version__", "__globals__", "StimTrig", "__header__"):
             b = x
-    return [['StimTrig', stim_trig_raw], [b, mat[b]]]
+    return [stim_trig_raw, mat[b]]
+# Returns a list with 2 elements
+# Element [0] is the StimTrig dictionary entry
+# Element [1] is the Sch_wav (or Schmitt for file 1) dictionary entry
 
-print(open_matlab_file('654508_rec02.mat')[0][1][0][0][4])
+StimTrig_Time = []
+StimTrig_Stimuli = []
+Sch_wav_Time = []
+
+file_name = '654508_rec02_f05.mat'
+
+# Pulls the timestamps in StimTrig into a list
+for x in open_matlab_file(file_name)[0][0][0][4]:
+    StimTrig_Time.append(x[0])
+
+# Pulls the order of stimuli from StimTrif into a list
+for x in open_matlab_file(file_name)[0][0][0][5]:
+    StimTrig_Stimuli.append(x[0])
+
+# Pulls the timestamps of Sch_wav into a list
+for x in open_matlab_file(file_name)[1][0][0][4]:
+    Sch_wav_Time.append(x[0])
+
+print(StimTrig_Time)
+print(StimTrig_Stimuli)
+print(Sch_wav_Time)
+
+# This commented section was used to pull the data into excel
+# It will write into a csv file (which can be converted to excel externally)
+# It writes the first column of the csv file to contain the Sch_wav timestamps
+# There is then a 2 column break following this
+# It then writes the StimTrig timestamps into a column, and the StimTrig stimuli into the next
+
+# writing_file = open("test_graph.csv", "r+", newline='')
+# john_cena = csv.writer(writing_file)
+# counter = 0
+# for x in Sch_wav_Time:
+#     try:
+#         john_cena.writerow([x, "", "", StimTrig_Time[counter], StimTrig_Stimuli[counter]])
+#     except IndexError:
+#         john_cena.writerow([x])
+#     counter += 1
