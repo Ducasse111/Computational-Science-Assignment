c7a135d01914e383f5e0be5233eacfb43a62ffe5
jvo9 <vo-0008@jmss.vic.edu.au>
2015-09-21 15:33:27 +1000

    - 'Jeffrey_Open_File.py' now only opens files from the folder Data_Input. The python file must be in the same folder as Data_Input, and all files that are to be accessed are to be put in Data_Input. Don't change the name, else you need to modify inside the python file.

--- Jeffrey_Open_File.py
+++ Jeffrey_Open_File.py
@@ -1,19 +1,27 @@
-# Scipy is necessry for opening the MatLab files
+# Scipy is necessary for opening the MatLab files
 # MatPlotLib is not used yet, but may be used to plot the data
 # CSV is so that the data could be written to excel
+
 import scipy.io as scio
 import matplotlib.pyplot as mpl
 import csv
+import os
+
+# Changes the directory that is being worked in. This allows loadmat to access files in the Data folder
+# loadmat only looks in the current directory, thus this function changes the current directory to the Data folder
+os.chdir(os.getcwd()+"\\Data_Input")
 
 # Function task the name of the MatLab files (including the .mat at the end)
 def open_matlab_file(matlab_filename):
     # headers of relevant data = 'StimTrig', b
-    mat = scio.loadmat(matlab_filename)
+    try:
+        mat = scio.loadmat(matlab_filename, appendmat=True)
+    except FileNotFoundError:
+        # Find better way to deal with errors
+        print("File not found")
+        quit()
     stim_trig_raw = mat['StimTrig']
-    for x in mat.keys():
-        # I assumed that Sch_wav and Schmitt would be used interchangebly thoughout the files
-        if x not in ("__version__", "__globals__", "StimTrig", "__header__"):
-            b = x
+    b = scio.whosmat(matlab_filename)[0][0]
     return [stim_trig_raw, mat[b]]
 # Returns a list with 2 elements
 # Element [0] is the StimTrig dictionary entry
@@ -23,7 +31,7 @@ StimTrig_Time = []
 StimTrig_Stimuli = []
 Sch_wav_Time = []
 
-file_name = '654508_rec02_f05.mat'
+file_name = '660804_rec03_all'
 
 # Pulls the timestamps in StimTrig into a list
 for x in open_matlab_file(file_name)[0][0][0][4]:
@@ -37,9 +45,25 @@ for x in open_matlab_file(file_name)[0][0][0][5]:
 for x in open_matlab_file(file_name)[1][0][0][4]:
     Sch_wav_Time.append(x[0])
 
-print(StimTrig_Time)
-print(StimTrig_Stimuli)
-print(Sch_wav_Time)
+print("StimTrig_Time", StimTrig_Time)
+print("StimTrig_Stimuli", StimTrig_Stimuli)
+print("Sch_wav_Time", Sch_wav_Time)
+
+# Playing around with frequency
+
+# average_frequency = float(len(Sch_wav_Time)/(float(Sch_wav_Time[-1])-float(Sch_wav_Time[1])))
+# print(average_frequency)
+# print(1/average_frequency)
+#
+# Total = 0
+# for i,x in enumerate(Sch_wav_Time):
+#     try:
+#         Total += (float(Sch_wav_Time[i+1])-float(x))
+#     except IndexError:
+#         break
+#
+# print((Total/(len(Sch_wav_Time)))-(1/average_frequency))
+# print((Total/(len(Sch_wav_Time)-1))-(1/average_frequency))
 
 # This commented section was used to pull the data into excel
 # It will write into a csv file (which can be converted to excel externally)
