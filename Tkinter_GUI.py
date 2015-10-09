298dd57a76dffe0a2649dff40d5e0b2754b3f9da
Ducasse111 <wit0001@jmss.vic.edu.au>
2015-10-09 15:10:46 +1100

    -added trial  counter

--- Tkinter_GUI.py
+++ Tkinter_GUI.py
@@ -9,45 +9,45 @@ class Application(tk.Frame):
         tk.Frame.__init__(self, master)
         self.grid()
         self.activefile = "Unloaded"
+        self.NumberOfTrials = "Unknown"
         self.add_widgets()
-
 
     def add_widgets(self):
         self.quitButton = tk.Button(self, text = "Quit", command = self.quit)
         self.Graphbutton = tk.Button(self, text = "Graph selected trial", command = self.Create_Graph)
         self.BrowseButton = tk.Button(self, text = "Browse", command = self.Browse)
         self.statustext = tk.Text(self, width = 30, height = 5)
+        self.Trialtext = tk.Text(self, width = 30, height = 1)
         self.TrialEntry = tk.Entry(self)
         self.TrialEntry.bind('<Return>', self.Create_Graph)
         self.TrialLabel = tk.Label(self, text = "Trial Number:")
-        self.Graphbutton.grid(column=0, row=0)
-        self.TrialLabel.grid(column=0, row=1)
-        self.TrialEntry.grid(column=0, row=2)
+        self.Graphbutton.grid(column = 0, row = 0)
+        self.TrialLabel.grid(column = 0, row = 1)
+        self.TrialEntry.grid(column = 0, row = 2)
         self.BrowseButton.grid(column = 0, row = 3)
-        self.statustext.grid(column = 0, row = 4)
-        self.quitButton.grid(column=0, row=5)
+        self.Trialtext.grid(column = 0, row = 4)
+        self.statustext.grid(column = 0, row = 5)
+        self.quitButton.grid(column = 0, row = 6)
         self.statustext.insert("1.0","Waiting for input.\n")
-
+        self.Trialtext.insert("1.0","Number of trials: " + self.NumberOfTrials + "\n")
 
     def Browse(self):
         self.activefile = filedialog.askopenfilename()
         self.activefile = self.activefile.split("/")
-        print(self.activefile)
         self.activefile = self.activefile[len(self.activefile)-1]
         if self.activefile != "":
             self.statustext.insert("1.0","Opened file: \n\"" + self.activefile + "\"\n")
-
+            self.StimTrig_Stimuli, self.StimTrig_Time, self.Sch_wav_Time, self.trial_dictionary,\
+                self.Sch_wav_Time_Trialled, self.number_of_trials = Graph.open_matlab_file(self.activefile)
+            self.Trialtext.insert("1.0","Number of trials: " + str(self.number_of_trials) + "\n")
 
     def Create_Graph(self, event):
         TrialNum = self.TrialEntry.get()
         self.statustext.insert("1.0","Attempting to graph trial "+TrialNum+" \nof file " + self.activefile + "\n")
         if self.activefile != None:
-            StimTrig_Stimuli, StimTrig_Time, Sch_wav_Time, trial_dictionary, Sch_wav_Time_Trialled, number_of_trials\
-                                                                            = Graph.open_matlab_file(self.activefile)
 
             try:
-                Graph.trial_graphs(Sch_wav_Time_Trialled, StimTrig_Stimuli, StimTrig_Time, trial_dictionary, TrialNum)
-
+                Graph.trial_graphs(self.Sch_wav_Time_Trialled, self.StimTrig_Stimuli, self.StimTrig_Time, self.trial_dictionary, TrialNum)
             except IndexError:
                 self.statustext.insert("1.0", "No such trial.\n")
 
