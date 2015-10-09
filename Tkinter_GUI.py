4223db4ed2ca3ec458c85a6b9248993cfe94dcab
Ducasse111 <wit0001@jmss.vic.edu.au>
2015-10-09 14:44:28 +1100

    - added return key functionality for graphing - replaced label with textbox for displaying system status - added error checking for index out of range - other stuff

--- Tkinter_GUI.py
+++ Tkinter_GUI.py
@@ -1,58 +1,64 @@
 import tkinter as tk
 from tkinter import filedialog
 
-import matplotlib.pyplot as mpl
-
 import Graph
 
-##bullsshit to make it commit
 
 class Application(tk.Frame):
     def __init__(self, master = None):
         tk.Frame.__init__(self, master)
         self.grid()
         self.activefile = None
-        self.statustext = "Waiting"
         self.add_widgets()
+
 
 
     def add_widgets(self):
         self.quitButton = tk.Button(self, text = "Quit", command = self.quit)
         self.Graphbutton = tk.Button(self, text = "Graph selected trial", command = self.Create_Graph)
         self.BrowseButton = tk.Button(self, text = "Browse", command = self.Browse)
-        self.statuslabel = tk.Label(self, text = self.statustext)
+        self.statustext = tk.Text(self, width = 30, height = 5)
         self.TrialEntry = tk.Entry(self)
+        self.TrialEntry.bind('<Return>', self.Create_Graph)
         self.TrialLabel = tk.Label(self, text = "Trial Number:")
-        self.quitButton.grid(column=0, row=0)
-        self.Graphbutton.grid(column=0, row=1)
-        self.TrialLabel.grid(column=0, row=2)
-        self.TrialEntry.grid(column=1, row=2)
+        self.Graphbutton.grid(column=0, row=0)
+        self.TrialLabel.grid(column=0, row=1)
+        self.TrialEntry.grid(column=0, row=2)
         self.BrowseButton.grid(column = 0, row = 3)
-        self.statuslabel.grid(column = 0, row = 4)
+        self.statustext.grid(column = 0, row = 4)
+        self.quitButton.grid(column=0, row=5)
+        self.statustext.insert("1.0","Waiting for input.\n")
 
-    def UpdateStatus(self):
-        self.statuslabel = tk.Label(self, text = self.statustext)
-        self.statuslabel.grid(column = 0, row = 4)
 
     def Browse(self):
         self.activefile = filedialog.askopenfilename()
-        self.statustext = "Opened file : \"" + self.activefile + "\""
-        self.UpdateStatus()
+        self.activefile = self.activefile.split("/")
+        print(self.activefile)
+        self.activefile = self.activefile[len(self.activefile)-1]
+        if self.activefile != "":
+            self.statustext.insert("1.0","Opened file: \"" + self.activefile + "\"\n")
 
-    def Create_Graph(self):
+
+    def Create_Graph(self, event):
+        self.statustext.insert("1.0","Graphing " + self.activefile + "\n")
         TrialNum = self.TrialEntry.get()
         print(TrialNum)
         #mpl.close()
         if self.activefile != None:
             StimTrig_Stimuli, StimTrig_Time, Sch_wav_Time, trial_dictionary, Sch_wav_Time_Trialled, number_of_trials = Graph.open_matlab_file(self.activefile)
-            Graph.trial_graphs(Sch_wav_Time_Trialled, StimTrig_Stimuli, StimTrig_Time, trial_dictionary, TrialNum)
-            self.statustext = "Graphing"
-            self.UpdateStatus()
+
+            try:
+                Graph.trial_graphs(Sch_wav_Time_Trialled, StimTrig_Stimuli, StimTrig_Time, trial_dictionary, TrialNum)
+
+            except IndexError:
+                self.statustext.insert("1.0", "No such trial.\n")
+
         else:
-            self.statustext = "No File Selected"
-            self.UpdateStatus()
+            self.statustext.insert("1.0","No File Selected\n")
+
 
 
 app = Application()
-app.master.title("Tkinter GUI V0.1")
+app.master.title("Tkinter GUI V0.5")
+app.master.minsize(217,217)
 app.mainloop()
