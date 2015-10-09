b438e8f921a742374587536500093adccf1528aa
jvo9 <vo-0008@jmss.vic.edu.au>
2015-10-09 13:53:00 +1100

    iehtq943yt98742ftIEAhf

--- Graph.py
+++ Graph.py
@@ -4,11 +4,6 @@ import sys
 import scipy.io as scio
 import matplotlib.pyplot as mpl
 
-
-
-
-
-
 # Changes the directory that is being worked in. This allows loadmat to access files in the Data folder
 # loadmat only looks in the current directory, thus this function changes the current directory to the Data folder
 if sys.platform == ("win32" or "cygwin"):
@@ -29,6 +24,7 @@ def open_matlab_file(matlab_filename):
             quit()
     b = scio.whosmat(matlab_filename)[0][0]
 
+    number_trials = 0
     stim_time = []
     stim_code = []
     firing = []
@@ -41,6 +37,7 @@ def open_matlab_file(matlab_filename):
             stim_code.append(x[0])
         else:
             stim_code.append(0)
+            number_trials += 1
     for x in mat[b][0][0][4]:
         firing.append(x[0])
     counter = 1
@@ -60,7 +57,7 @@ def open_matlab_file(matlab_filename):
                 Temporary_Key += 1
         except KeyError:
             break
-    return stim_code, stim_time, firing, separate_dictionary, trialled_firing
+    return stim_code, stim_time, firing, separate_dictionary, trialled_firing, number_trials
 
 # Function to find frequency of firing 200 milliseconds before stimuli applied
 # Iterate through firing list by popping items off
@@ -85,12 +82,10 @@ def freqeuncy_before_stimulus(firing_stamp, stimulus_type, stim_timestamp):
 def trial_graphs(sch_wav_trials, stimuli, stimuli_time, dictionary_trial, user_selection = "1"):
     user_selection = str(user_selection)
     mpl.ioff()
-    if user_selection == "break":
-        return
-    elif user_selection == "quit":
-        quit()
-    elif user_selection == "1":
-        mpl.figure(num="Trial", figsize=[14,7])
+    mpl.figure(num="Trial", figsize=[14,7])
+    mpl.show(block=False)
+    if user_selection == "1":
+        mpl.cla()
         for x in sch_wav_trials[int(user_selection)-1]:
             mpl.plot([x,x], [0,10], "r-")
         mpl.plot(stimuli_time[0:dictionary_trial[1]+1], stimuli[0:dictionary_trial[1]+1], 'ko', ms=6)
@@ -98,9 +93,8 @@ def trial_graphs(sch_wav_trials, stimuli, stimuli_time, dictionary_trial, user_s
             mpl.annotate(s=str(x), xy=(stimuli_time[i],x), xytext=(stimuli_time[i],10.2), color='0.2', size=13, weight="bold")
         mpl.xlim(xmin=0, xmax=stimuli_time[dictionary_trial[1]])
         mpl.draw()
-        mpl.show(block=False)
     else:
-        mpl.figure(num="Trial", figsize=[14,7])
+        mpl.cla()
         for x in sch_wav_trials[int(user_selection)-1]:
             mpl.plot([x,x], [0,10], "r-")
         mpl.plot(stimuli_time[dictionary_trial[int(user_selection)-1]:dictionary_trial[int(user_selection)]+1],
@@ -110,16 +104,8 @@ def trial_graphs(sch_wav_trials, stimuli, stimuli_time, dictionary_trial, user_s
             mpl.annotate(s=str(x), xy=(stimuli_time[i+dictionary_trial[int(user_selection)-1]],x), xytext=(stimuli_time[i+dictionary_trial[int(user_selection)-1]],10.2), color='0.2', size=13, weight="bold")
         mpl.xlim(xmin=stimuli_time[dictionary_trial[int(user_selection)-1]], xmax=stimuli_time[dictionary_trial[int(user_selection)]])
         mpl.draw()
-        mpl.show(block=False)
 
-StimTrig_Stimuli, StimTrig_Time, Sch_wav_Time, trial_dictionary, Sch_wav_Time_Trialled = open_matlab_file('660806_rec03_all')
-print(len(Sch_wav_Time))
-# the extra 50 comes from all the 0's (reset stimuli)
-
-# a = freqeuncy_before_stimulus(Sch_wav_Time, StimTrig_Stimuli, StimTrig_Time)
-# print(a[9][0], a[7][0])
-
-# trial_graphs(Sch_wav_Time_Trialled, StimTrig_Stimuli, StimTrig_Time, trial_dictionary)
+# a,b,c,d,e,f = open_matlab_file("660806_rec03_all")
 
 # total = 0
 # restriction = 0
@@ -142,19 +128,3 @@ print(len(Sch_wav_Time))
 # print(total)
 # print(len(Sch_wav_Time))
 # print(highest)
-
-# This commented section was used to pull the data into excel
-# It will write into a csv file (which can be converted to excel externally)
-# It writes the first column of the csv file to contain the Sch_wav timestamps
-# There is then a 2 column break following this
-# It then writes the StimTrig timestamps into a column, and the StimTrig stimuli into the next
-
-# writing_file = open("test_graph.csv", "r+", newline='')
-# john_cena = csv.writer(writing_file)
-# counter = 0
-# for x in Sch_wav_Time:
-#     try:
-#         john_cena.writerow([x, "", "", StimTrig_Time[counter], StimTrig_Stimuli[counter]])
-#     except IndexError:
-#         john_cena.writerow([x])
-#     counter += 1
