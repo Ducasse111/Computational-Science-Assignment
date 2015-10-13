1cd9939c1fcf3cba054ea6baae88c0996fe5b607
jreq1 <req0001@jmss.vic.edu.au>
2015-10-13 14:23:03 +1100

    Fix to the current broken graphing, rescaling not working yet.

--- Tkinter_GUI.py
+++ Tkinter_GUI.py
@@ -9,7 +9,6 @@ import Graph
 
 # Class Reference: A
 class Application(tk.Frame):
-
     # Function Reference: A
     def __init__(self, master = None):
         tk.Frame.__init__(self, master)
@@ -17,9 +16,14 @@ class Application(tk.Frame):
         self.activefile = "Unloaded"
         self.NumberOfTrials = "Unknown"
         self.add_widgets()
+        self.bind("<Configure>", self.on_resize)
 
     # Function Reference: B
     def add_widgets(self):
+        for row in range(0, 8):
+            tk.Grid.rowconfigure(self, row, weight=1)
+        tk.Grid.columnconfigure(self, 0, weight=1)
+
         self.quitButton = tk.Button(self, text="Quit", command=self.quit)
         self.Graphbutton = tk.Button(self, text="Graph selected trial", command=self.create_graph)
         self.BrowseButton = tk.Button(self, text="Browse", command=self.browse)
@@ -28,18 +32,22 @@ class Application(tk.Frame):
         self.TrialEntry = tk.Entry(self)
         self.TrialEntry.bind('<Return>', self.create_graph)
         self.TrialLabel = tk.Label(self, text="Trial Number:")
-        self.Graphbutton.grid(column=0, row=0)
-        self.TrialLabel.grid(column=0, row=1)
-        self.TrialEntry.grid(column=0, row=2)
-        self.BrowseButton.grid(column=0, row=3)
-        self.Trialtext.grid(column=0, row=4)
-        self.statustext.grid(column=0, row=5)
-        self.quitButton.grid(column=0, row=6)
-        self.statustext.insert("1.0", "Waiting for input.\n")
+
+        self.Graphbutton.grid(column=0, row=0, sticky="E"+"W")
+        self.TrialLabel.grid(column=0, row=1, sticky="E"+"W")
+        self.TrialEntry.grid(column=0, row=2, sticky="E"+"W")
+        self.BrowseButton.grid(column=0, row=3, sticky="E"+"W")
+        self.Trialtext.grid(column=0, row=4, sticky="E"+"W")
+        self.statustext.grid(column=0, row=5, sticky="E"+"W")
+        self.quitButton.grid(column=0, row=6, sticky="E"+"W")
+
+        self.statustext.insert("1.0", "Waiting for input.\n",)
         self.Trialtext.insert("1.0", "Number of trials: " + self.NumberOfTrials + "\n")
 
-        self.panel = tk.Label(self, image=None)
-        self.panel.grid(column=0, row=7)
+        self.raw_image = None
+        self.image = None
+        self.panel = tk.Label(self, image=self.image)
+        self.panel.grid(column=0, row=7, sticky="N"+"S"+"E"+"W")
 
     # Function Reference: C
     def browse(self):
@@ -60,22 +68,32 @@ class Application(tk.Frame):
             try:
                 x = Graph.trial_graphs(self.Sch_wav_Time_Trialled, self.StimTrig_Stimuli, self.StimTrig_Time,
                                    self.trial_dictionary, TrialNum)
-                img = Image.open(io.BytesIO(x))
-                tkimage = ImageTk.PhotoImage(img)
+                self.img = Image.open(io.BytesIO(x))
+                self.raw_img = self.img
+                #self.image = self.img.resize((self.panel.winfo_width(), self.panel.winfo_height()), Image.ANTIALIAS)
+                self.image = ImageTk.PhotoImage(self.img)
 
-                self.panel.image = tkimage
-                self.panel.configure(image=tkimage)
+                self.panel.image = self.image
+                self.panel.configure(image=self.image)
             except IndexError:
                 self.statustext.insert("1.0", "No such trial.\n")
 
         else:
             self.statustext.insert("1.0", "No File Selected\n")
 
+    def on_resize(self, event):
+        if self.raw_image is not None:
+            #self.image = self.raw_image.resize((self.panel.winfo_width(), self.panel.winfo_height()), Image.ANTIALIAS)
+            self.image = ImageTk.PhotoImage(self.img)
+
+            self.panel.image = self.image
+            self.panel.configure(image=self.image)
+
 # Section Reference: B
-try:
-    app = Application()
-    app.master.title("Tkinter GUI V0.5")
-    app.master.minsize(217, 217)
-    app.mainloop()
-except Exception as error:
-    print("Error Caught: Code: 01-B", error)
+#try:
+app = Application()
+app.master.title("Tkinter GUI V0.5")
+app.master.minsize(217, 217)
+app.mainloop()
+#except Exception as error:
+#    print("Error Caught: Code: 01-B", error)
